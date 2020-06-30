0145f45ceef7429826337d5fd933b29597511e4c
Lorinc Ambrozy <lambrozy@logmein.com>
2020-06-30 14:03:57 +0200

    Initial release

---
+++ src/AsKeyIndicator/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace AsKeyIndicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new TransparentCheckBox();
+            this.keyChecker = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "NumLock";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // keyChecker
+            // 
+            this.keyChecker.Tick += new System.EventHandler(this.keyChecker_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(76, 22);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.6D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TransparentCheckBox checkBox1;
+        private System.Windows.Forms.Timer keyChecker;
+    }
+}
+
---
+++ src/AsKeyIndicator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsKeyIndicator
+{
+    public partial class Form1 : Form
+    {
+        private bool mouseDown;
+        private Point lastLocation;
+        private TransparentPanel panel1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyChecker_Tick(object sender, EventArgs e)
+        {
+            checkBox1.Checked = Control.IsKeyLocked(Keys.NumLock);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1 = new TransparentPanel();
+            panel1.MouseMove += new MouseEventHandler(panel1_MouseMove);
+            panel1.MouseUp += new MouseEventHandler(panel1_MouseUp);
+            panel1.MouseDown += new MouseEventHandler(panel1_MouseDown);
+            panel1.MouseDoubleClick += new MouseEventHandler(panel1_MouseDoubleClick);
+            panel1.AutoSize = false;
+            Size = checkBox1.Size;
+            panel1.Size = checkBox1.Size;
+            
+            Controls.Add(panel1);
+
+            panel1.BringToFront();
+
+            panel1.Location = new Point(0, 0);
+            keyChecker.Start();
+            Location = new Point(1400, 0);
+        }
+
+        private void panel1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+            panel1.Size = panel1.Parent.ClientSize;
+            panel1.MaximumSize = panel1.Parent.ClientSize;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                Location = new Point(
+                    (Location.X - lastLocation.X) + e.X, (Location.Y - lastLocation.Y) + e.Y);
+
+                Update();
+            }
+        }
+    }
+}
